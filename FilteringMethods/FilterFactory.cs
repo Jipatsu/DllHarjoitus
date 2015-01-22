f4be574874d89f8d192794d1d4125c45325aa968
Jipatsu <jipatsu@gmail.com>
2015-01-22 18:43:34 +0200

    uus yritys

---
+++ FilteringMethods/FilterFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringMethods
+{
+    public enum FilterTypes
+    {
+        Median, Average
+    }
+
+    public class FilterFactory
+    {
+
+        public static Filter CreateFilter(FilterTypes type, int lenght)
+        {
+            Filter filter = null;
+            switch (type)
+            {
+                case FilterTypes.Average:
+                    filter = new AverageFilter(lenght);
+                    break;
+
+                case FilterTypes.Median:
+                    filter = new MedianFilter(lenght);
+                    break;
+
+                default:
+                    // throw some exception
+                    break;
+            }
+            return filter;
+        }
+
+    }
+}
